14f1c9a28c76a27b20d579599c0b082d22f1d8ff
Om Saraswat <sharmaom27022004@gmail.com>
2025-04-01 04:28:54 +0530

    sample

---
+++ Sample/app.js
@@ -0,0 +1,9 @@
+
+const {sum} = require("./sum.js")
+var a = 10 ;
+var b =20 ;
+
+// console.log(a+b)
+// console.log(globalThis)
+
+sum(a,b);
---
+++ Sample/sum.js
@@ -0,0 +1,10 @@
+
+var a = 1;
+var b = 2;
+var x = " hello ! baba "
+// console.log(x);
+function sum(a,b){
+   console.log(a+b);
+}
+
+module.exports = {sum: sum};
